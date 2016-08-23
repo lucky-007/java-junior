2f64022b32786d38c5ac5f1d29fc872d95e7eaee
daniil <gorbachevdaniil3003@gmail.com>
2016-08-23 17:04:19 +0300

    Iteration01 done. Refactored code in Logger with switch.

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,11 +1,23 @@
 package com.acme.edu;
 
 public class Logger {
-    public static void log(int message) {
-        System.out.println("primitive: " + message);
-    }
-
-    public static void log(byte message) {
-        System.out.println("primitive: " + message);
+    public static void log(Object message) {
+        switch (message.getClass().getName()) {
+            case "java.lang.Boolean":
+            case "java.lang.Byte":
+            case "java.lang.Integer":
+                System.out.println("primitive: " + message);
+                break;
+            case "java.lang.Character":
+                System.out.println("char: " + message);
+                break;
+            case "java.lang.String":
+                System.out.println("string: " + message);
+                break;
+            case "java.lang.Object":
+                System.out.println("reference: " + message);
+                break;
+        }
     }
 }
+
