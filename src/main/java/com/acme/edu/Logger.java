341e694c8d64bc8ae1e80eb3921566e809d65cab
Alexey <eremen.ai123@gmail.com>
2016-08-25 16:49:33 +0300

    Documentation and a bit refactoring.

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,7 +1,5 @@
 package com.acme.edu;
 
-import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
-
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,21 +8,28 @@ public class Logger {
 
     private Decorator decorator;
     private Writer writer;
-    private Message savedMessage = null;
-    private int stringsCount = 1;
+    private Message savedMessage;
+    private int stringsCount;
+
+    public Logger(Decorator decorator, Writer writer) {
+        this.decorator = decorator;
+        this.writer = writer;
+        savedMessage = null;
+        stringsCount = 1;
+    }
 
     private void processSequenceData(Message message) {
         switch(message.getType()) {
             case "java.lang.Byte":
                 if(Byte.MAX_VALUE - Math.abs((byte)savedMessage.getValue()) < (byte)message.getValue()) {
-                    printSavedObjectAndSaveNewOne(message);
+                    decoratePrintSavedMessageAndSaveNewOne(message);
                 } else {
                     savedMessage.setValue ( (byte)((byte) savedMessage.getValue() + (byte)message.getValue()));
                 }
                 break;
             case "java.lang.Integer":
                 if(Integer.MAX_VALUE - Math.abs((int)savedMessage.getValue()) < (int)message.getValue()) {
-                    printSavedObjectAndSaveNewOne(message);
+                    decoratePrintSavedMessageAndSaveNewOne(message);
                 } else {
                     savedMessage.setValue ((int) savedMessage.getValue() + (int)message.getValue());
                 }
@@ -35,21 +40,17 @@ public class Logger {
                 if(m.matches()) {
                     savedMessage.setValue( message.getValue() + " (x" + (++stringsCount) + ")");
                 } else {
-                    printSavedObjectAndSaveNewOne(message);
+                    decoratePrintSavedMessageAndSaveNewOne(message);
                     stringsCount = 1;
                 }
                 break;
             default:
-                printSavedObjectAndSaveNewOne(message);
+                decoratePrintSavedMessageAndSaveNewOne(message);
                 break;
         }
     }
-    public Logger(Decorator decorator, Writer writer) {
-        this.decorator = decorator;
-        this.writer = writer;
-    }
 
-    private void printSavedObjectAndSaveNewOne(Message message) {
+    private void decoratePrintSavedMessageAndSaveNewOne(Message message) {
         if (savedMessage != null) {
             decorator.decorate(savedMessage);
             writer.write(savedMessage);
@@ -66,7 +67,25 @@ public class Logger {
      * and adds the result of last logging request.
      */
     public void stopLogging() {
-        printSavedObjectAndSaveNewOne(null);
+        decoratePrintSavedMessageAndSaveNewOne(null);
+    }
+
+    /**
+     * Overloaded method for logging for different types of inputs parameters.
+     * @param mes Object...
+     */
+    public void log (Object... mes) {
+        for (Object element: mes) {
+            log(element);
+        }
+    }
+
+    private void processMessage(Message message) {
+        if (checkWithPrevMessage(message)) {
+            processSequenceData(message);
+        } else {
+            decoratePrintSavedMessageAndSaveNewOne(message);
+        }
     }
 
     /**
@@ -75,43 +94,33 @@ public class Logger {
      */
     public void log (Object mes) {
         Message message = new Message(mes);
-        if (checkWithPrevMessage(message)) {
-            processSequenceData(message);
-        } else {
-            printSavedObjectAndSaveNewOne(message);
-        }
+        processMessage(message);
     }
 
-    public void log (Object... mes) {
-        for (Object element: mes) {
-            log(element);
-        }
-    }
-
+    /**
+     * Overloaded method for logging for different types of inputs parameters.
+     * @param mes int[]
+     */
     public void logIntArray(int[] mes) {
         Message message = new Message(mes);
-        if (checkWithPrevMessage(message)) {
-            processSequenceData(message);
-        } else {
-            printSavedObjectAndSaveNewOne(message);
-        }
+        processMessage(message);
     }
 
+    /**
+     * Overloaded method for logging for different types of inputs parameters.
+     * @param mes int[][]
+     */
     public void logIntArray(int[][] mes) {
         Message message = new Message(mes);
-        if (checkWithPrevMessage(message)) {
-            processSequenceData(message);
-        } else {
-            printSavedObjectAndSaveNewOne(message);
-        }
+        processMessage(message);
     }
 
+    /**
+     * Overloaded method for logging for different types of inputs parameters.
+     * @param mes int[][][][]
+     */
     public void logIntArray(int[][][][] mes) {
         Message message = new Message(mes);
-        if (checkWithPrevMessage(message)) {
-            processSequenceData(message);
-        } else {
-            printSavedObjectAndSaveNewOne(message);
-        }
+        processMessage(message);
     }
 }
