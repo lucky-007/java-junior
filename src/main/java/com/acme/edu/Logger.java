3d435d058f1a68514694131c7b335e146772cd7d
daniil <gorbachevdaniil3003@gmail.com>
2016-08-25 15:09:58 +0300

    Refactoring to OOP design done.

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,5 +1,7 @@
 package com.acme.edu;
 
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -11,11 +13,42 @@ public class Logger {
     private Message savedMessage = null;
     private int stringsCount = 1;
 
+    private void processSequenceData(Message message) {
+        switch(message.getType()) {
+            case "java.lang.Byte":
+                if(Byte.MAX_VALUE - Math.abs((byte)savedMessage.getValue()) < (byte)message.getValue()) {
+                    printSavedObjectAndSaveNewOne(message);
+                } else {
+                    savedMessage.setValue ( (byte)((byte) savedMessage.getValue() + (byte)message.getValue()));
+                }
+                break;
+            case "java.lang.Integer":
+                if(Integer.MAX_VALUE - Math.abs((int)savedMessage.getValue()) < (int)message.getValue()) {
+                    printSavedObjectAndSaveNewOne(message);
+                } else {
+                    savedMessage.setValue ((int) savedMessage.getValue() + (int)message.getValue());
+                }
+                break;
+            case "java.lang.String":
+                Pattern p = Pattern.compile("^"+ message.getValue() +"($| \\(x\\d+\\)$)");
+                Matcher m = p.matcher((String) savedMessage.getValue());
+                if(m.matches()) {
+                    savedMessage.setValue( message.getValue() + " (x" + (++stringsCount) + ")");
+                } else {
+                    printSavedObjectAndSaveNewOne(message);
+                    stringsCount = 1;
+                }
+                break;
+            default:
+                printSavedObjectAndSaveNewOne(message);
+                break;
+        }
+    }
     public Logger(Decorator decorator, Writer writer) {
         this.decorator = decorator;
         this.writer = writer;
     }
-//todo just save
+
     private void printSavedObjectAndSaveNewOne(Message message) {
         if (savedMessage != null) {
             decorator.decorate(savedMessage);
@@ -36,62 +69,6 @@ public class Logger {
         printSavedObjectAndSaveNewOne(null);
     }
 
-//    public static void log (byte mes) {
-//        if (checkWithPrevMessage(mes)) {
-//            if(Byte.MAX_VALUE - (byte) savedMessage < mes) {
-//                printSavedObjectAndSaveNewOne(mes);
-//            } else {
-//                savedMessage = (byte)((byte) savedMessage + mes);
-//            }
-//        } else {
-//            printSavedObjectAndSaveNewOne(mes);
-//        }
-//    }
-//
-//    /**
-//     * Overloaded method for logging for different types of inputs parameters.
-//     * @param mes integer
-//     */
-//    public static void log (int mes) {
-//        if (checkWithPrevMessage(mes)) {
-//            if(Integer.MAX_VALUE - (int) savedMessage < mes) {
-//                printSavedObjectAndSaveNewOne(mes);
-//            } else {
-//                savedMessage = (int) savedMessage + mes;
-//            }
-//        } else {
-//            printSavedObjectAndSaveNewOne(mes);
-//        }
-//    }
-//
-//    /**
-//     * Overloaded method for logging for different types of inputs parameters.
-//     * @param mes char
-//     */
-//    public static void log (char mes) {
-//        printSavedObjectAndSaveNewOne(mes);
-//    }
-//
-//    /**
-//     * Overloaded method for logging for different types of inputs parameters.
-//     * @param mes String
-//     */
-//    public static void log (String mes) {
-//        if(checkWithPrevMessage(mes)) {
-//            Pattern p = Pattern.compile("^"+mes+"($| \\(x\\d+\\)$)");
-//            Matcher m = p.matcher((String) savedMessage);
-//            if(m.matches()) {
-//                savedMessage = mes + " (x" + (++stringsCount) + ")";
-//            } else {
-//                printSavedObjectAndSaveNewOne(mes);
-//                stringsCount = 1;
-//            }
-//        } else {
-//            printSavedObjectAndSaveNewOne(mes);
-//        }
-//    }
-
-
     /**
      * Overloaded method for logging for different types of inputs parameters.
      * @param mes Object
@@ -105,66 +82,36 @@ public class Logger {
         }
     }
 
-    private void processSequenceData(Message message) {
-        switch(message.getType()) {
-            case "java.lang.Byte":
-                if(Byte.MAX_VALUE - (byte) savedMessage.getValue() < (byte)message.getValue()) {
-                    printSavedObjectAndSaveNewOne(message);
-                } else {
-                    savedMessage.setValue ( (byte)((byte) savedMessage.getValue() + (byte)message.getValue()));
-                }
-                break;
-            case "java.lang.Integer":
-                if(Integer.MAX_VALUE - (int) savedMessage.getValue() < (int)message.getValue()) {
-                    printSavedObjectAndSaveNewOne(message);
-                } else {
-                    savedMessage.setValue ((int) savedMessage.getValue() + (int)message.getValue());
-                }
-                break;
-            case "java.lang.String":
-                Pattern p = Pattern.compile("^"+ message.getValue() +"($| \\(x\\d+\\)$)");
-                Matcher m = p.matcher((String) savedMessage.getValue());
-                if(m.matches()) {
-                    savedMessage.setValue( message.getValue() + " (x" + (++stringsCount) + ")");
-                } else {
-                    printSavedObjectAndSaveNewOne(message);
-                    stringsCount = 1;
-                }
-                break;
+    public void log (Object... mes) {
+        for (Object element: mes) {
+            log(element);
         }
     }
 
-//    /**
-//     * Overloaded method for logging for different types of inputs parameters.
-//     * @param mes two-dimensional array of ints
-//     */
-//    public static void log (int[][] mes) {
-//        printSavedObjectAndSaveNewOne(mes);
-//    }
-//
-//    /**
-//     * Overloaded method for logging for different types of inputs parameters.
-//     * @param mes four-dimensional array of ints
-//     */
-//    public static void log (int[][][][] mes) {
-//        printSavedObjectAndSaveNewOne(mes);
-//    }
-//
-//    /**
-//     * Overloaded method for logging for different types of inputs parameters.
-//     * @param mes vararg of Strings
-//     */
-//    public static void log (String... mes) {
-//        for (String element: mes) {
-//            log(element);
-//        }
-//    }
-//
-//    /**
-//     * Overloaded method for logging for different types of inputs parameters.
-//     * @param mes varargs of ints and one-dimensional array of them
-//     */
-//    public static void log (int... mes) {
-//        printSavedObjectAndSaveNewOne(mes);
-//    }
+    public void logIntArray(int[] mes) {
+        Message message = new Message(mes);
+        if (checkWithPrevMessage(message)) {
+            processSequenceData(message);
+        } else {
+            printSavedObjectAndSaveNewOne(message);
+        }
+    }
+
+    public void logIntArray(int[][] mes) {
+        Message message = new Message(mes);
+        if (checkWithPrevMessage(message)) {
+            processSequenceData(message);
+        } else {
+            printSavedObjectAndSaveNewOne(message);
+        }
+    }
+
+    public void logIntArray(int[][][][] mes) {
+        Message message = new Message(mes);
+        if (checkWithPrevMessage(message)) {
+            processSequenceData(message);
+        } else {
+            printSavedObjectAndSaveNewOne(message);
+        }
+    }
 }
--- src/test/java/com/acme/edu/iteration03/LoggerTest.java
+++ src/test/java/com/acme/edu/iteration03/LoggerTest.java
@@ -30,7 +30,7 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
     @Test
     public void shouldLogIntegersArray() throws IOException {
         //region when
-        logger.log(new int[] {-1, 0, 1});
+        logger.logIntArray(new int[] {-1, 0, 1});
         logger.stopLogging();
         //endregion
 
@@ -44,7 +44,7 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
     @Test
     public void shouldLogIntegersMatrix() throws IOException {
         //region when
-        logger.log(new int[][] {{-1, 0, 1}, {1, 2, 3}, {-1, -2, -3}});
+        logger.logIntArray(new int[][] {{-1, 0, 1}, {1, 2, 3}, {-1, -2, -3}});
         logger.stopLogging();
         //endregion
 
@@ -62,7 +62,7 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
     @Test
     public void shouldLogIntegersMulitidimentionalArray() throws IOException {
         //region when
-        logger.log(new int[][][][] {{{{0}}}});
+        logger.logIntArray(new int[][][][] {{{{0}}}});
         logger.stopLogging();
         //endregion
 
@@ -77,31 +77,31 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         //endregion
     }
 
-//    @Test
-//    public void shouldLogStringsWithOneMethodCall() throws IOException {
-//        //region when
-//        logger.log("str1", "string 2", "str 3");
-//        logger.stopLogging();
-//        //endregion
-//
-//        //region then
-//        assertSysoutContains("str1");
-//        assertSysoutContains("string 2");
-//        assertSysoutContains("str 3");
-//        //endregion
-//    }
-//
-//    @Test
-//    public void shouldLogIntegersWithOneMethodCall() throws IOException {
-//        //region when
-//        logger.log(-1, 0, 1, 3);
-//        logger.stopLogging();
-//        //endregion
-//
-//        //region then
-//        assertSysoutContains("3");
-//        //endregion
-//    }
+    @Test
+    public void shouldLogStringsWithOneMethodCall() throws IOException {
+        //region when
+        logger.log("str1", "string 2", "str 3");
+        logger.stopLogging();
+        //endregion
+
+        //region then
+        assertSysoutContains("str1");
+        assertSysoutContains("string 2");
+        assertSysoutContains("str 3");
+        //endregion
+    }
+
+    @Test
+    public void shouldLogIntegersWithOneMethodCall() throws IOException {
+        //region when
+        logger.log(-1, 0, 1, 3);
+        logger.stopLogging();
+        //endregion
+
+        //region then
+        assertSysoutContains("3");
+        //endregion
+    }
 
     @Test
     public void shouldCorrectDealWithIntegerOverflowWhenOneMethodCall() throws IOException {
